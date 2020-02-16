ae7e28917df37c2bd280e11aea5a5e21f3a5d24b
prince <bhartiprince.9971@gmail.com>
2020-02-16 13:51:50 +0530

    update

---
+++ MyFramework/PNNotification.m
@@ -0,0 +1,15 @@
+//
+//  PNNotification.m
+//  MyFramework
+//
+//  Created by Prince Bharti on 16/02/20.
+//  Copyright © 2020 Prince Bharti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@implementation PNNotification: NSObject
+
+
+
+@end
---
+++ MyFramework/SwitchView.swift
@@ -0,0 +1,26 @@
+//
+//  SwitchView.swift
+//  MyFramework
+//
+//  Created by Prince Bharti on 16/02/20.
+//  Copyright © 2020 Prince Bharti. All rights reserved.
+//
+
+import UIKit
+
+public class SwitchView: UIView {
+   
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public init(frame: CGRect,backgroundColor: UIColor){
+        super.init(frame: frame)
+        self.backgroundColor = backgroundColor
+    }
+    
+}
